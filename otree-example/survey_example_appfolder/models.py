3f6a9e7c23578e50f8da3b2e9cf42fa5fe2e3ce8
Ari-manius <147991895+Ari-manius@users.noreply.github.com>
2024-11-27 15:58:31 +0100

    final

--- otree-example/survey_example_appfolder/models.py
+++ otree-example/survey_example_appfolder/models.py
@@ -9,6 +9,9 @@ from otree.api import (
     currency_range,
 )
 
+#import random for group assignment
+import random 
+
 author = 'Me'
 doc = 'Test Survey for Seminar'
 
@@ -18,7 +21,9 @@ class Constants(BaseConstants):
     num_rounds = 1
 
 class Subsession(BaseSubsession):
-    pass
+     def creating_session(self):
+        for p in self.get_players():
+            p.group_assignment = random.Random().randint(0, 1)
 
 class Group(BaseGroup):
     #we will only come to the group class when we look at advanced methods
@@ -44,3 +49,20 @@ class Player(BasePlayer):
     )
 
     yes_no_question = models.BooleanField(default=False)
+
+    group_assignment = models.IntegerField(initial=-1)
+    #this is the most important feature of this file. We can collect all the variables used on the html pages here
+
+    duck = models.IntegerField(initial=-999, label="Please select the value that is most fitting:")
+
+    goose = models.IntegerField(initial=-999, label="Please select the value that is most fitting:")
+
+    #varibles for recording of screen width/height
+    screen_height = models.IntegerField(initial=-999)
+    screen_width = models.IntegerField(initial=-999)
+
+    #Variables for  the popup questions
+    popout_question = models.IntegerField(blank=True)
+    popout_yes = models.StringField(blank=True)
+    popout_no = models.StringField(blank=True)
+    time_popout = models.StringField(initial='-999')
--- otree-example/survey_example_appfolder/pages.py
+++ otree-example/survey_example_appfolder/pages.py
@@ -9,16 +9,44 @@ from .models import Constants, Player
 
 class Welcome(Page):
     form_model = Player
+    form_fields = ['screen_height', 'screen_width']
 
 class DemoPage(Page):
     form_model = Player
-    form_fields = ['numeric_question', 'float_question' , 'opentext_question', 'mc_question', 'yes_no_question']
+    form_fields = ['numeric_question', 
+                   'float_question' , 
+                   'opentext_question', 
+                   'mc_question', 
+                   'yes_no_question']
+
+class DemoPage4_group1(Page):
+    def is_displayed(self):
+        return self.player.group_assignment == 0
+    form_model = Player
+    form_fields = ["goose"]
+
+class DemoPage4_group2(Page):
+    def is_displayed(self):
+        return self.player.group_assignment == 1
+    form_model = Player 
+    form_fields = ["duck"]
+
+class PopupQuestion(Page):
+    form_model = Player
+    form_fields = ['popout_question', 'popout_yes', 'popout_no', 'time_popout']
 
 class EndPage(Page):
     #style: this is a good example of the style 'CamelCase' that one normally uses for classes
     form_model = Player
+    def vars_for_template(self):
+        '''this is another function by otree which allows you to "send" variables
+        to html files if you need to access them from there'''
+        return {"group_assignment": safe_json(self.player.group_assignment)}
 
 #Here we define in which ordering we want the pages to be shown. We always start with a Welcome page and end with an End page.
 page_sequence = [Welcome,
-                DemoPage,           
+                DemoPage,
+                DemoPage4_group1,
+                DemoPage4_group2, 
+                PopupQuestion,             
                 EndPage]
